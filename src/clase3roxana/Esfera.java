2806e47631db61381daa52a76c91199b5853fe97
roxy71 <roxana71janet@gmail.com>
2017-10-26 21:53:15 -0300

    Esta clase calcula el volumen de una esfera

---
+++ src/clase3roxana/Esfera.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3roxana;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LAB03
+ */
+public class Esfera 
+{
+     public static void main(String[] args)
+    {
+        float radio, volumen;
+        final float PI=3.14f;//final define una constante
+    
+      
+       
+       //Convertimos el string a entero antes de guardarlo
+        radio = Float.parseFloat(JOptionPane.showInputDialog("Ingrese su salario"));
+      
+        
+        if (radio>0) 
+        {
+        volumen = (4/3)*PI*(radio*radio*radio);
+         JOptionPane.showMessajeDialog(null,"El volumen de su esfera es: "+volumen);
+        }
+    }
+}   
+
